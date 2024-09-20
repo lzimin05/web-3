044ff68e6d69e32421272f40a1eb1da05b85cd27
Lenya <lzimin05@mail.ru>
2024-09-20 16:17:00 +0300

    new commit

--- projects/atoi/main.go
+++ projects/atoi/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
+
+func SqrtDigit(a string) {
+	for _, i := range a {
+		if unicode.IsDigit(i) {
+			c := int(i - 48)
+			fmt.Print(c * c)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var a string
+	fmt.Scan(&a)
+	SqrtDigit(a)
 }
--- projects/math/main.go
+++ projects/math/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+var k, p, v float64 = 1296, 6, 6
+
+func T() float64 {
+	return 6 / W()
+}
+func W() float64 {
+	return math.Sqrt(k / M())
+}
+func M() float64 {
+	return p * v
+}
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	fmt.Println(T())
 }
